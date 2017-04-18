a606d3d2e9be2de3b17cae7b1c6c43fdc9209f2d
Cichar <363655056@qq.com>
2017-04-18 20:22:41 +0800

    新增知乎用户模型

--- models.py
+++ models.py
@@ -3,10 +3,43 @@
 __author__ = 'Cichar'
 __version__ = '0.2'
 
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, TEXT, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+class ZhiHuUserInfo(Base):
+    """
+
+    知乎用户数据
+
+    """
+
+    __tablename__ = 'zhhuserinfos'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), default=u'未知')
+    headline = Column(String(128), default=u'未知')
+    location = Column(String(64), default=u'未知')
+    description = Column(TEXT(), default=u'未知')
+    voteup_count = Column(Integer)
+    thanked_count = Column(Integer)
+    favorited_count = Column(Integer)
+    logs_count = Column(Integer)
+    question_count = Column(Integer)
+    url_token = Column(String(64), default=u'未知')
+    follower_count = Column(Integer)
+    following_count = Column(Integer)
+    business = Column(String(64), default=u'未知')
+    employment = Column(String(64), default=u'未知')
+    company = Column(String(64), default=u'未知')
+    update_time = Column(DateTime())
+    updata_flag = Column(Boolean, default=True)
+    crawl_flag = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return 'ZhiHuUser %r' % self.name
 
 
 class PlayList(Base):
